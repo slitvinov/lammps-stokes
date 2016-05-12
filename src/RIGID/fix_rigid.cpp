fb4d369d82ad0daf74a416a7bd4459ad1810f098
Sergey Litvinov <slitvinov@gmail.com>
2016-05-13 00:40:16 +0200

    Update every timestep

--- src/RIGID/fix_rigid.cpp
+++ src/RIGID/fix_rigid.cpp
@@ -833,7 +833,8 @@ void FixRigid::initial_integrate(int vflag)
     vcm[ibody][2] += dtfm * fcm[ibody][2] * fflag[ibody][2];
 
     // update xcm by full step
-    int posUpdateFlag = (update->ntimestep % 100) == 0;
+    // int posUpdateFlag = (update->ntimestep % 100) == 0;
+    int posUpdateFlag    = 1;
     if (posUpdateFlag) {
       xcm[ibody][0] += dtv * vcm[ibody][0];
       xcm[ibody][1] += dtv * vcm[ibody][1];
